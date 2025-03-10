511e5594e6f9da959fcbf78e3cb57e2d1e022c2a
OdionOseiwe <oseiweifebhor@gmail.com>
2025-03-10 11:04:36 +0100

    fixing issues in the Agent

--- frontend/src/Pages/UploadJobsPage.jsx
+++ frontend/src/Pages/UploadJobsPage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import { PlusCircle, Dot } from "lucide-react";
-import CustomInput from "../components/CustomInput.jsx";
-import NavBar from "../components/NavBar.jsx";
+import CustomInput from "../components/CustomInput";
+import NavBar from "../components/NavBar";
 
 function UploadJobsPage() {
   const [activeDescription, setActiveDescription] = useState();
